cc41fa5f5df186fb6b53424bd398afd65165a1bd
Tien Dang <tiendang@Tiens-MacBook-Pro.local>
2023-02-17 15:47:37 +0700

    fix bug tensorboard

--- train_fasterrcnn.py
+++ train_fasterrcnn.py
@@ -7,7 +7,10 @@ from utils import collate_fn
 from torch.utils.data import DataLoader
 from pytorch_lightning import Trainer, seed_everything
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
-from pytorch_lightning.loggers.tensorboard import TensorBoardLogger
+from pytorch_lightning.loggers import TensorBoardLogger
+
+# import warnings
+# warnings.filterwarnings("ignore")
 
 if __name__ == '__main__':
     # Seed
@@ -16,8 +19,7 @@ if __name__ == '__main__':
     # Data
     train_data = TacoDataset(root=config.ROOT, 
                             annFile=config.TRAIN_DATA, 
-                            bbformat='xyxy', 
-                            transform=config.transform)
+                            bbformat='xyxy')
     
     val_data = TacoDataset(root=config.ROOT, 
                             annFile=config.VAL_DATA, 
@@ -40,24 +42,23 @@ if __name__ == '__main__':
 
     # Callback
     ckpt = ModelCheckpoint(dirpath=config.CHECKPOINT_DIR,monitor='map', save_top_k=2, mode='max')
-    early_stopping = EarlyStopping(monitor="map", mode='max', check_finite=True)
-    tensorboard = TensorBoardLogger(save_dir=config.TENSORBOARD["DIR"], 
-                                    name=config.TENSORBOARD["NAME"], 
-                                    version=config.TENSORBOARD["VERSION"])
+    early_stopping = EarlyStopping(monitor="map", mode='max', check_finite=True, patience=5)
+    tensorboard = TensorBoardLogger(save_dir=config.TENSORBOARD["DIR"], name=config.TENSORBOARD["NAME"], version=config.TENSORBOARD["VERSION"])
 
     # Trainer
     trainer = Trainer(accelerator=config.ACCELERATOR, 
                     check_val_every_n_epoch=config.EVAL_EVERY_EPOCH,
-                    gradient_clip_val=1.0, 
+                    gradient_clip_val=1.0,
+                    accumulate_grad_batches=8, 
                     max_epochs=config.NUM_EPOCHS, 
                     enable_checkpointing=True, 
-                    deterministic=True,
+                    deterministic=False,
                     default_root_dir=config.CHECKPOINT_DIR, 
-                    callbacks=[ckpt, early_stopping, tensorboard])
+                    callbacks=[ckpt, early_stopping], logger=tensorboard)
 
     trainer.fit(model=model,
                 train_dataloaders=train_dataloader,
                 val_dataloaders=val_dataloader,
                 ckpt_path = config.CONTINUE_TRAINING) 
 
-    print(" == Finish Training == ")               
+    print(" == Finish Training == ")
